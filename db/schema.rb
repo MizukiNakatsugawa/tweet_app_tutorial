14ad5bdc89b554c93945aae10483199abbaf3dd0
MizukiNakatsugawa <mailaddress.mizuki.nakatsugawa@gmail.com>
2020-03-23 14:56:09 +0900

    rails g model User and add User validates

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_044107) do
+ActiveRecord::Schema.define(version: 2020_03_23_055350) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
