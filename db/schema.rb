d67c173175caa5aea4bdacb2747d34bb953c6327
MizukiNakatsugawa <mailaddress.mizuki.nakatsugawa@gmail.com>
2020-03-23 15:36:22 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_055350) do
+ActiveRecord::Schema.define(version: 2020_03_23_063418) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_055350) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
   end
 
 end
